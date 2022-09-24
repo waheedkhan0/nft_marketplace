88c2b153e383376ad36cc22bc84a98d6f5ea2ea6
Waheed Khan <waheedkhan05@gmail.com>
2022-09-24 17:00:49 +0500

    basic setup and setting up some componenets

---
+++ components/SubInfo.js
@@ -0,0 +1,51 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const NFTTitle = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  )
+}
+
+export const ETHPrice = () => {
+    return (
+      <View>
+        <Text>EthPrice</Text>
+      </View>
+    )
+  }
+
+  export const ImageCmp = () => {
+    return (
+      <View>
+        <Text>Image Cmp</Text>
+      </View>
+    )
+  }
+
+
+  export const EndDate = () => {
+    return (
+      <View>
+        <Text>EndDate</Text>
+      </View>
+    )
+  }
+
+  export const People = () => {
+    return (
+      <View>
+        <Text>People</Text>
+      </View>
+    )
+  }
+
+  export const SubInfo = () => {
+    return (
+      <View>
+        <Text>SubInfo</Text>
+      </View>
+    )
+  }
